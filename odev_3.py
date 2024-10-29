58b5263a9e0605a674268137eadb987330adbcb3
sudebalkis <sudebalkiiss@gmail.com>
2024-10-29 19:16:29 +0300

    Create odev_3.py

---
+++ odev_3.py
@@ -0,0 +1,10 @@
+import math
+
+# Kullanıcıdan bir sayı al
+sayi = int(input("Bir sayı girin: "))
+
+# Karekök ve faktöriyel hesaplama
+karekök = math.sqrt(sayi)
+faktöriyel = math.factorial(sayi)
+
+print(f"Karekök: {karekök}, Faktöriyel: {faktöriyel}")
